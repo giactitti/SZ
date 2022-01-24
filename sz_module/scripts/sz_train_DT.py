132015ba659889362c07d3f231303ddf4bc5b9c0
Giacomo Titti <giacomotitti@gmail.com>
2022-01-24 07:56:34 +0100

    Update sz_train_DT.py

--- sz_module/scripts/sz_train_DT.py
+++ sz_module/scripts/sz_train_DT.py
@@ -506,7 +506,7 @@ class DTAlgorithm(QgsProcessingAlgorithm):
         #print self.classes
         #aucv=roc_auc_score(self.y_v, self.scores_v, None)
         #auct=roc_auc_score(self.y_t, self.scores_t, None)
-        r=roc_auc_score(y_true, scores, None)
+        r=roc_auc_score(y_true, scores)
 
         fig=plt.figure()
         lw = 2
