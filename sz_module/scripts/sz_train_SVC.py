7fd679e23702ec230b20447c604a1ec3b4ff20e3
Giacomo Titti <giacomotitti@gmail.com>
2022-01-24 08:04:58 +0100

    Update sz_train_SVC.py

--- sz_module/scripts/sz_train_SVC.py
+++ sz_module/scripts/sz_train_SVC.py
@@ -508,7 +508,7 @@ class SVCAlgorithm(QgsProcessingAlgorithm):
         #print self.classes
         #aucv=roc_auc_score(self.y_v, self.scores_v, None)
         #auct=roc_auc_score(self.y_t, self.scores_t, None)
-        r=roc_auc_score(y_true, scores, None)
+        r=roc_auc_score(y_true, scores)
 
         fig=plt.figure()
         lw = 2
