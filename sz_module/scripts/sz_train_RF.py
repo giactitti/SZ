d116432c83ed6ffc8a5a2b4b9f2171b8d603bf5e
Giacomo Titti <giacomotitti@gmail.com>
2022-01-24 07:59:12 +0100

    Update sz_train_RF.py

--- sz_module/scripts/sz_train_RF.py
+++ sz_module/scripts/sz_train_RF.py
@@ -507,7 +507,7 @@ class RFAlgorithm(QgsProcessingAlgorithm):
         #print self.classes
         #aucv=roc_auc_score(self.y_v, self.scores_v, None)
         #auct=roc_auc_score(self.y_t, self.scores_t, None)
-        r=roc_auc_score(y_true, scores, None)
+        r=roc_auc_score(y_true, scores)
 
         fig=plt.figure()
         lw = 2
